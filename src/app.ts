0a78b0554df397bdf9714c61cf989b9e5886ca1b
Sandeep <sandepten@gmail.com>
2023-03-04 20:23:21 +0530

    add: port variable

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 
 const app = express()
+const PORT = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(4000, () => {
-  console.log('Server started on port 4000')
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
 })
